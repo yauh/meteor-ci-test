f866cb41d1a0b7a2b964be271344fbbacf3c9580
Stephan Hochhaus <stephan@yauh.de>
2015-01-07 12:51:35 +0100

    single quotes only

--- app/client/app.js
+++ app/client/app.js
@@ -1,15 +1,15 @@
 // counter starts at 0
-Session.setDefault("counter", 0);
+Session.setDefault('counter', 0);
 
 Template.button.helpers({
     counter: function () {
-        return Session.get("counter");
+        return Session.get('counter');
     }
 });
 
 Template.button.events({
     'click button': function () {
         // increment the counter when button is clicked
-        Session.set("counter", Session.get("counter") + 1);
+        Session.set('counter', Session.get('counter') + 1);
     }
 });
--- app/tests/mocha/client/sampleClientTest.js
+++ app/tests/mocha/client/sampleClientTest.js
@@ -1,7 +1,7 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
-    describe("a group of tests", function(){
-      it("should respect equality", function(){
+    describe('a group of tests', function(){
+      it('should respect equality', function(){
         chai.assert.equal(5,5);
       });
     });
