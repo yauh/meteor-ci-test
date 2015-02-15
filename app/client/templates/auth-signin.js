2708d6290d3660d06933c54cd655d5f5efe69c1c
Stephan Hochhaus <stephan@yauh.de>
2015-02-15 14:50:16 +0100

    ready for another CI test

--- app/client/templates/auth-signin.js
+++ app/client/templates/auth-signin.js
@@ -1,46 +1,46 @@
 var ERRORS_KEY = 'signinErrors';
 
-Template.signin.created = function() {
-  Session.set(ERRORS_KEY, {});
+Template.signin.created = function () {
+    Session.set(ERRORS_KEY, {});
 };
 
 Template.signin.helpers({
-  errorMessages: function() {
-    return _.values(Session.get(ERRORS_KEY));
-  },
-  errorClass: function(key) {
-    return Session.get(ERRORS_KEY)[key] && 'error';
-  }
+    errorMessages: function () {
+        return _.values(Session.get(ERRORS_KEY));
+    },
+    errorClass: function (key) {
+        return Session.get(ERRORS_KEY)[key] && 'error';
+    }
 });
 
 Template.signin.events({
-  'submit': function(event, template) {
-    event.preventDefault();
-    
-    var email = template.$('[name=email]').val();
-    var password = template.$('[name=password]').val();
-    
-    var errors = {};
+    'submit': function (event, template) {
+        event.preventDefault();
 
-    if (! email) {
-      errors.email = 'Email is required';
-    }
+        var email = template.$('[name=email]').val();
+        var password = template.$('[name=password]').val();
 
-    if (! password) {
-      errors.password = 'Password is required';
+        var errors = {};
+
+        if (!email) {
+            errors.email = 'Email is required';
+        }
+
+        if (!password) {
+            errors.password = 'Password is required';
+        }
+
+        Session.set(ERRORS_KEY, errors);
+        if (_.keys(errors).length) {
+            return;
+        }
+
+        Meteor.loginWithPassword(email, password, function (error) {
+            if (error) {
+                return Session.set(ERRORS_KEY, {'none': error.reason});
+            }
+
+            Router.go('home');
+        });
     }
-    
-    Session.set(ERRORS_KEY, errors);
-    if (_.keys(errors).length) {
-      return;
-    }
-    
-    Meteor.loginWithPassword(email, password, function(error) {
-      if (error) {
-        return Session.set(ERRORS_KEY, {'none': error.reason});
-      }
-      
-      Router.go('home');
-    });
-  }
 });
--- app/server/publish.js
+++ app/server/publish.js
@@ -1,10 +1,16 @@
 Meteor.publish('publicLists', function() {
-  return Lists.find({userId: {$exists: false}});
+  return Lists.find({
+    userId: {
+      $exists: false
+    }
+  });
 });
 
 Meteor.publish('privateLists', function() {
   if (this.userId) {
-    return Lists.find({userId: this.userId});
+    return Lists.find({
+      userId: this.userId
+    });
   } else {
     this.ready();
   }
@@ -13,5 +19,7 @@ Meteor.publish('privateLists', function() {
 Meteor.publish('todos', function(listId) {
   check(listId, String);
 
-  return Todos.find({listId: listId});
+  return Todos.find({
+    listId: listId
+  });
 });
